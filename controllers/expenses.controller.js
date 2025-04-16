80fcec212ed18b33807cea1d3acc324501997a30
arslan <arslankhan863345@gmail.com>
2025-04-16 02:49:03 -0700

    query filtering added

--- controllers/expenses.controller.js
+++ controllers/expenses.controller.js
@@ -3,10 +3,62 @@ const { Expense } = require('../models/expense.model.js')
 
 const expensesGetController = async (req, res) => {
     try {
-        const allExpenses = await Expense.find(req.query);
+
+        // Filtering
+
+        const condition = {}
+        if (req.query.category) {
+            condition.category = req.query.category
+        }
+
+        if (req.query.amountGreaterThan && req.query.amountLessThan) {
+            condition.$and = [
+                { amount: { $gte: req.query.amountGreaterThan } },
+                { amount: { $gte: req.query.amountLessThan } }
+            ]
+        } else if (req.query.amountGreaterThan) {
+            condition.amount = { $gte: req.query.amountGreaterThan }
+        } else if (req.query.amountLessThan) {
+            condition.amount = { $lte: req.query.amountLessThan }
+        }
+
+        let query = Expense.find(condition);
+
+        //Sorting
+        if (req.query.sort) {
+            let sortBy = req.query.sort.split(',').join(' ');
+            query = query.sort(sortBy);
+        } else {
+            query = query.sort('amount')
+        }
+
+        // Limiting the fields
+        if (req.query.fiels) {
+            let fields = req.query.fields.split(',').join(' ');
+            query = query.select(fields)
+        }
+
+        // Pagination
+        let page = req.query.page * 1 || 1;
+        let limit = req.query.limit * 1 || 10;
+        let skip = (page - 1) * limit;
+
+        let numberRecords = await Expense.countDocuments();
+
+        if (skip < numberRecords) {
+            query = query.skip(skip).limit(limit);
+        } else {
+            return res.status(404).json({
+                status: 'failed',
+                message: 'Page does not exist'
+            })
+        }
+
+        const allExpenses = await query;
         // console.log(allExpenses)
         res.status(200).json({
             status: 'Success',
+            length: allExpenses.length,
             data: allExpenses
         })
     } catch (error) {
@@ -14,7 +66,7 @@ const expensesGetController = async (req, res) => {
     }
 };
 
-const expensesPostController = async ( req, res) => {
+const expensesPostController = async (req, res) => {
     try {
         const newExpense = await Expense.create({
             amount: req.body.amount,
@@ -44,13 +96,13 @@ const expensesPutController = async (req, res) => {
         })
 
     } catch (error) {
-        console.log(error)        
+        console.log(error)
     }
 }
 
 const expensesDeleteController = async (req, res) => {
     try {
-        await Expense.deleteOne({_id: req.params.id})
+        await Expense.deleteOne({ _id: req.params.id })
         res.status(204).json({
             status: "success"
         });
